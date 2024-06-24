90e69cf84edae6b25208b86b5115e4302a664fc1
Susmi701 <susmiels590@gmail.com>
2024-06-24 15:27:51 +0530

    Send gmail confirmation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,15 +67,18 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  # config.mailer = 'Devise::Mailer'
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-  address: 'smtp.example.com',
-  port: 587,
-  user_name: 'Susmi',
-  password: 'Pass@12345',
-  authentication: 'plain',
-  enable_starttls_auto: true
-}
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+                                          address: 'smtp.gmail.com',
+                                          port: 587,
+                                          domain: 'gmail.com',
+                                          user_name: 'susmiels590@gmail.com',
+                                          password: 'dqjfgycodphehzem',
+                                          authentication: 'login',
+                                          enable_starttls_auto: true
+                                        }
 end
